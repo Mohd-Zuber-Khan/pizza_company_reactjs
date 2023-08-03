8d3a055e1fb9b6036d5035bfc83755e66433177e
zuber <kzuberkhan0786@gmail.com>
2023-08-03 14:24:28 +0530

    pizzacompany

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,8 +1,39 @@
 import React from 'react'
+import data from "../data"
+import pizzaData from '../data'
+// import "./Style.css"
+import "../App.css"
 
 function Menu() {
   return (
-    <div>Menu</div>
+    <>
+      <section>
+        <div className='menuContent'>
+        <h2>OUR MENU</h2>
+
+        <p>Authentic italian  cuisine, 6 creative dishes to choose from . all from our store owen. all organic. all delicious</p>
+        </div>
+        <div className='pizzaArea'>
+        {
+          pizzaData.map((data) => {
+            return (
+              <div id={data.soldOut ? "gray":"normal"}>
+                <div className='pizzaImage'>
+                  <img src={data.photoName}></img>
+                </div>
+                <div className='pizzaContent'>
+                  <h2>{data.name}</h2>
+                  <p>{data.ingredients}</p>
+                  <h3>{(data.soldOut) ? "Sold Out " : data.price}</h3>
+                </div>
+
+              </div>
+            )
+          })
+        }
+        </div>
+      </section>
+    </>
   )
 }
 
